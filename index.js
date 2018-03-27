c26d3c2864ebdf4fbba596dfbdd124eb5cc69ff7
sd1998 <shashvat51@gmail.com>
2018-03-27 20:04:54 +0530

    Bug fixes

--- index.js
+++ index.js
@@ -75,48 +75,40 @@ rule.minute = 0
 
 var scheduler = schedule.scheduleJob(rule, function() {
     var date = new Date();
-    var data = getSubjectData();
-    if (data != null) {
-        if (date.getDay() == 1) {
-            incrClassHeld('/subject/MED209', data.MED209.ClassHeld + 1);
-            incrClassHeld('/subject/CSD101', data.CSD101.ClassHeld + 1);
-            incrClassHeld('/subject/EED102', data.EED102.ClassHeld + 1);
-            incrClassHeld('/subject/EED205', data.EED205.ClassHeld + 1);
-        } else if (date.getDay() == 2) {
-            incrClassHeld('/subject/CED101', data.CED101.ClassHeld + 1);
-            incrClassHeld('/subject/CSD201', data.CSD201.ClassHeld + 1);
-            incrClassHeld('/subject/MAT104', data.MAT104.ClassHeld + 1);
-            incrClassHeld('/subject/PHY102', data.PHY102.ClassHeld + 1);
-            incrClassHeld('/subject/CSD204', data.CSD204.ClassHeld + 1);
-        } else if (date.getDay() == 3) {
-            incrClassHeld('/subject/MED209', data.MED209.ClassHeld + 1);
-            incrClassHeld('/subject/CSD101', data.CSD101.ClassHeld + 1);
-            incrClassHeld('/subject/EED102', data.EED102.ClassHeld + 1);
-            incrClassHeld('/subject/EED208', data.EED208.ClassHeld + 1);
-            incrClassHeld('/subject/EED204', data.EED204.ClassHeld + 1);
-        } else if (date.getDay() == 4) {
-            incrClassHeld('/subject/CED101', data.CED101.ClassHeld + 1);
-            incrClassHeld('/subject/CSD201', data.CSD201.ClassHeld + 1);
-            incrClassHeld('/subject/MAT104', data.MAT104.ClassHeld + 1);
-            incrClassHeld('/subject/PHY102', data.PHY102.ClassHeld + 1);
-            incrClassHeld('/subject/EED205', data.EED205.ClassHeld + 1);
-        } else if (date.getDay() == 5) {
-            incrClassHeld('/subject/EED208', data.EED208.ClassHeld + 1);
-            incrClassHeld('/subject/EED204', data.EED204.ClassHeld + 1);
-            incrClassHeld('/subject/CSD204', data.CSD204.ClassHeld + 1);
-        }
-    }
-});
-
-function getSubjectData() {
-    var subject = firebase.database().ref("/subject/").once('value', function(snapshot) {
+    firebase.database().ref("/subject/").once('value', function(snapshot) {
         if (snapshot.exists()) {
-            return snapshot.val();
-        } else {
-            return null;
+            var data = snapshot.val();
+            if (date.getDay() == 1) {
+                incrClassHeld('/subject/MED209/', data.MED209.ClassHeld + 1);
+                incrClassHeld('/subject/CSD101/', data.CSD101.ClassHeld + 1);
+                incrClassHeld('/subject/EED102/', data.EED102.ClassHeld + 1);
+                incrClassHeld('/subject/EED205/', data.EED205.ClassHeld + 1);
+            } else if (date.getDay() == 2) {
+                incrClassHeld('/subject/CED101/', data.CED101.ClassHeld + 1);
+                incrClassHeld('/subject/CSD201/', data.CSD201.ClassHeld + 1);
+                incrClassHeld('/subject/MAT104/', data.MAT104.ClassHeld + 1);
+                incrClassHeld('/subject/PHY102/', data.PHY102.ClassHeld + 1);
+                incrClassHeld('/subject/CSD204/', data.CSD204.ClassHeld + 1);
+            } else if (date.getDay() == 3) {
+                incrClassHeld('/subject/MED209/', data.MED209.ClassHeld + 1);
+                incrClassHeld('/subject/CSD101/', data.CSD101.ClassHeld + 1);
+                incrClassHeld('/subject/EED102/', data.EED102.ClassHeld + 1);
+                incrClassHeld('/subject/EED208/', data.EED208.ClassHeld + 1);
+                incrClassHeld('/subject/EED204/', data.EED204.ClassHeld + 1);
+            } else if (date.getDay() == 4) {
+                incrClassHeld('/subject/CED101/', data.CED101.ClassHeld + 1);
+                incrClassHeld('/subject/CSD201/', data.CSD201.ClassHeld + 1);
+                incrClassHeld('/subject/MAT104/', data.MAT104.ClassHeld + 1);
+                incrClassHeld('/subject/PHY102/', data.PHY102.ClassHeld + 1);
+                incrClassHeld('/subject/EED205/', data.EED205.ClassHeld + 1);
+            } else if (date.getDay() == 5) {
+                incrClassHeld('/subject/EED208/', data.EED208.ClassHeld + 1);
+                incrClassHeld('/subject/EED204/', data.EED204.ClassHeld + 1);
+                incrClassHeld('/subject/CSD204/', data.CSD204.ClassHeld + 1);
+            }
         }
     });
-}
+});
 
 function incrClassHeld(classUrl, classHeld) {
     firebase.database().ref(classUrl).set({ ClassHeld: classHeld });
