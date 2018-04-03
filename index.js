26794193f626fe9ed2dea9dbe447dbcfcd9145d1
sd1998 <shashvat51@gmail.com>
2018-04-03 18:07:45 +0530

    Class timings changed

--- index.js
+++ index.js
@@ -41,32 +41,37 @@ function incrClass() {
         if (snapshot.exists()) {
             var data = snapshot.val();
             if (date.getDay() == 1) {
-                incrClassHeld('/subject/MED209/', data.MED209.ClassHeld + 1);
-                incrClassHeld('/subject/CSD101/', data.CSD101.ClassHeld + 1);
+                incrClassHeld('/subject/MAT104/', data.MAT104.ClassHeld + 1);
+                incrClassHeld('/subject/EED204/', data.EED204.ClassHeld + 1);
                 incrClassHeld('/subject/EED102/', data.EED102.ClassHeld + 1);
-                incrClassHeld('/subject/EED205/', data.EED205.ClassHeld + 1);
+                incrClassHeld('/subject/EED207/', data.EED207.ClassHeld + 1);
+                incrClassHeld('/subject/CSD201/', data.CSD201.ClassHeld + 1);
             } else if (date.getDay() == 2) {
                 incrClassHeld('/subject/CED101/', data.CED101.ClassHeld + 1);
                 incrClassHeld('/subject/CSD201/', data.CSD201.ClassHeld + 1);
-                incrClassHeld('/subject/MAT104/', data.MAT104.ClassHeld + 1);
-                incrClassHeld('/subject/PHY102/', data.PHY102.ClassHeld + 1);
-                incrClassHeld('/subject/CSD204/', data.CSD204.ClassHeld + 1);
-            } else if (date.getDay() == 3) {
                 incrClassHeld('/subject/MED209/', data.MED209.ClassHeld + 1);
+                incrClassHeld('/subject/PHY102/', data.PHY102.ClassHeld + 1);
+                incrClassHeld('/subject/EED205/', data.EED205.ClassHeld + 1);
+            } else if (date.getDay() == 3) {
+                incrClassHeld('/subject/MAT104/', data.MAT104.ClassHeld + 1);
                 incrClassHeld('/subject/CSD101/', data.CSD101.ClassHeld + 1);
                 incrClassHeld('/subject/EED102/', data.EED102.ClassHeld + 1);
-                incrClassHeld('/subject/EED208/', data.EED208.ClassHeld + 1);
+                incrClassHeld('/subject/EED205/', data.EED205.ClassHeld + 1);
                 incrClassHeld('/subject/EED204/', data.EED204.ClassHeld + 1);
+                incrClassHeld('/subject/EED207/', data.EED207.ClassHeld + 1);
             } else if (date.getDay() == 4) {
                 incrClassHeld('/subject/CED101/', data.CED101.ClassHeld + 1);
                 incrClassHeld('/subject/CSD201/', data.CSD201.ClassHeld + 1);
-                incrClassHeld('/subject/MAT104/', data.MAT104.ClassHeld + 1);
-                incrClassHeld('/subject/PHY102/', data.PHY102.ClassHeld + 1);
-                incrClassHeld('/subject/EED205/', data.EED205.ClassHeld + 1);
+                incrClassHeld('/subject/MED209/', data.MED209.ClassHeld + 1);
+                incrClassHeld('/subject/EED102/', data.EED102.ClassHeld + 1);
             } else if (date.getDay() == 5) {
-                incrClassHeld('/subject/EED208/', data.EED208.ClassHeld + 1);
-                incrClassHeld('/subject/EED204/', data.EED204.ClassHeld + 1);
+                incrClassHeld('/subject/PHY102/', data.PHY102.ClassHeld + 1);
+                incrClassHeld('/subject/MAT104/', data.MAT104.ClassHeld + 1);
                 incrClassHeld('/subject/CSD204/', data.CSD204.ClassHeld + 1);
+                incrClassHeld('/subject/CSD101/', data.CSD101.ClassHeld + 1);
+                incrClassHeld('/subject/EED205/', data.EED205.ClassHeld + 1);
+            } else if (date.getDay() == 6) {
+                incrClassHeld('/subject/MAT104/', data.MAT104.ClassHeld + 1);
             }
         }
     });
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             $(".csd101").hide();
             $(".eed102").hide();
             $(".csd201").hide();
-            $(".eed208").hide();
+            $(".eed207").hide();
             $(".eed204").hide();
             $(".mat104").hide();
             $(".phy102").hide();
@@ -23,7 +23,7 @@ $(document).ready(function() {
                             var subjectData = snapshot.val();
                             if (subjectData.CED101) {
                                 init(".ced101", "#ced101AttePercentage", "#ced101ProgressBar", subjectData.CED101.Attendance, subjectClass.CED101.ClassHeld);
-                                if ((date.getDay() == 2 || date.getDay() == 4) && ((date.getHours() == 22) || (date.getHours() == 23 && date.getMinutes() == 0))) {
+                                if ((date.getDay() == 2 || date.getDay() == 4) && date.getHours() == 20) {
                                     initButton("#ced101AtteMark", subjectData.CED101.TimeStamp, '/users/' + user.uid + '/classes/CED101', subjectClass.CED101.ClassHeld + 1);
                                     $("#ced101AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/CED101', subjectData.CED101.Attendance + 1, "#ced101AtteMark", subjectClass.CED101.ClassHeld);
@@ -35,7 +35,7 @@ $(document).ready(function() {
                             }
                             if (subjectData.MED209) {
                                 init(".med209", "#med209AttePercentage", "#med209ProgressBar", subjectData.MED209.Attendance, subjectClass.MED209.ClassHeld);
-                                if ((date.getDay() == 1 || date.getDay() == 3) && ((date.getHours() == 21) || (date.getHours() == 22 && date.getMinutes() == 0))) {
+                                if ((date.getDay() == 2 && (date.getHours() == 22)) || (date.getDay() == 4 && (date.getHours() == 20))) {
                                     initButton("#med209AtteMark", subjectData.MED209.TimeStamp, '/users/' + user.uid + '/classes/MED209', subjectClass.MED209.ClassHeld + 1);
                                     $("#med209AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/MED209', subjectData.MED209.Attendance + 1, "#med209AtteMark", subjectClass.MED209.ClassHeld);
@@ -47,7 +47,7 @@ $(document).ready(function() {
                             }
                             if (subjectData.CSD101) {
                                 init(".csd101", "#csd101AttePercentage", "#csd101ProgressBar", subjectData.CSD101.Attendance, subjectClass.CSD101.ClassHeld);
-                                if ((date.getDay() == 1 || date.getDay() == 3) && ((date.getHours() == 21) || (date.getHours() == 22 && date.getMinutes() == 0))) {
+                                if ((date.getDay() == 3 && (date.getHours() == 21)) || (date.getDay() == 5 && (date.getHours() == 21))) {
                                     initButton("#csd101AtteMark", subjectData.CSD101.TimeStamp, '/users/' + user.uid + '/classes/CSD101', subjectClass.CSD101.ClassHeld + 1);
                                     $("#csd101AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/CSD101', subjectData.CSD101.Attendance + 1, "#csd101AtteMark", subjectClass.CSD101.ClassHeld);
@@ -59,7 +59,7 @@ $(document).ready(function() {
                             }
                             if (subjectData.EED102) {
                                 init(".eed102", "#eed102AttePercentage", "#eed102ProgressBar", subjectData.EED102.Attendance, subjectClass.EED102.ClassHeld);
-                                if ((date.getDay() == 1 || date.getDay() == 3) && ((date.getHours() == 22) || (date.getHours() == 23 && date.getMinutes() == 0))) {
+                                if (((date.getDay() == 1 || date.getDay() == 3) && date.getHours() == 21) || (date.getDay() == 3 && date.getHours() == 21)) {
                                     initButton("#eed102AtteMark", subjectData.EED102.TimeStamp, '/users/' + user.uid + '/classes/EED102', subjectClass.EED102.ClassHeld + 1);
                                     $("#eed102AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/EED102', subjectData.EED102.Attendance + 1, "#eed102AtteMark", subjectClass.EED102.ClassHeld);
@@ -71,7 +71,7 @@ $(document).ready(function() {
                             }
                             if (subjectData.CSD201) {
                                 init(".csd201", "#csd201AttePercentage", "#csd201ProgressBar", subjectData.CSD201.Attendance, subjectClass.CSD201.ClassHeld);
-                                if ((date.getDay() == 2 || date.getDay() == 4) && ((date.getHours() == 22) || (date.getHours() == 23 && date.getMinutes() == 0))) {
+                                if ((date.getDay() == 1 && date.getHours() == 22) || (date.getDay() == 2 && date.getHours() == 21) || (date.getDay() == 4 && date.getHours() == 21)) {
                                     initButton("#csd201AtteMark", subjectData.CSD201.TimeStamp, '/users/' + user.uid + '/classes/CSD201', subjectClass.CSD201.ClassHeld + 1);
                                     $("#csd201AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/CSD201', subjectData.CSD201.Attendance + 1, "#csd201AtteMark", subjectClass.CSD201.ClassHeld);
@@ -81,21 +81,21 @@ $(document).ready(function() {
                                     changeAttendaceButtonState("#csd201AtteMark");
                                 }
                             }
-                            if (subjectData.EED208) {
-                                init(".eed208", "#eed208AttePercentage", "#eed208ProgressBar", subjectData.EED208.Attendance, subjectClass.EED208.ClassHeld);
-                                if ((date.getDay() == 3 || date.getDay() == 5) && ((date.getHours() == 22) || (date.getHours() == 23 && date.getMinutes() == 0))) {
-                                    initButton("#eed208AtteMark", subjectData.EED208.TimeStamp, '/users/' + user.uid + '/classes/EED208', subjectClass.EED208.ClassHeld + 1);
-                                    $("#eed208AtteMark").on('click', function() {
-                                        markAttendance('/users/' + user.uid + '/classes/EED208', subjectData.EED208.Attendance + 1, "#eed208AtteMark", subjectClass.EED208.ClassHeld);
-                                        init(".eed208", "#eed208AttePercentage", "#eed208ProgressBar", subjectData.EED208.Attendance + 1, subjectClass.EED208.ClassHeld);
+                            if (subjectData.EED207) {
+                                init(".eed207", "#eed207AttePercentage", "#eed207ProgressBar", subjectData.EED207.Attendance, subjectClass.EED207.ClassHeld);
+                                if ((date.getDay() == 1 && (date.getHours() == 22)) || (date.getDay() == 3 && (date.getHours() == 22))) {
+                                    initButton("#eed207AtteMark", subjectData.EED207.TimeStamp, '/users/' + user.uid + '/classes/EED207', subjectClass.EED207.ClassHeld + 1);
+                                    $("#eed207AtteMark").on('click', function() {
+                                        markAttendance('/users/' + user.uid + '/classes/EED207', subjectData.EED207.Attendance + 1, "#eed207AtteMark", subjectClass.EED207.ClassHeld);
+                                        init(".eed207", "#eed207AttePercentage", "#eed207ProgressBar", subjectData.EED207.Attendance + 1, subjectClass.EED207.ClassHeld);
                                     });
                                 } else {
-                                    changeAttendaceButtonState("#eed208AtteMark");
+                                    changeAttendaceButtonState("#eed207AtteMark");
                                 }
                             }
                             if (subjectData.EED204) {
                                 init(".eed204", "#eed204AttePercentage", "#eed204ProgressBar", subjectData.EED204.Attendance, subjectClass.EED204.ClassHeld);
-                                if ((date.getDay() == 3 || date.getDay() == 5) && ((date.getHours() == 20) || (date.getHours() == 21 && date.getMinutes() == 0))) {
+                                if ((date.getDay() == 3 && date.getHours() == 20) || (date.getDay() == 1 && date.getHours() == 20)) {
                                     initButton("#eed204AtteMark", subjectData.EED204.TimeStamp, '/users/' + user.uid + '/classes/EED204', subjectClass.EED204.ClassHeld + 1);
                                     $("#eed204AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/EED204', subjectData.EED204.Attendance + 1, "#eed204AtteMark", subjectClass.EEd204.ClassHeld);
@@ -107,7 +107,7 @@ $(document).ready(function() {
                             }
                             if (subjectData.MAT104) {
                                 init(".mat104", "#mat104AttePercentage", "#mat104ProgressBar", subjectData.MAT104.Attendance, subjectClass.MAT104.ClassHeld);
-                                if ((date.getDay() == 2 || date.getDay() == 4) && ((date.getHours() == 21) || (date.getHours() == 22 && date.getMinutes() == 0))) {
+                                if ((date.getDay() == 1 && (date.getHours() == 20 || date.getHours() == 21 || date.getHours() == 22)) || (date.getDay() == 3 && (date.getHours() == 20 || date.getHours() == 21 || date.getHours() == 22)) || (date.getDay() == 5 && (date.getHours() == 21)) || (date.getDay() == 0 && (date.getHours() == 21))) {
                                     initButton("#mat104AtteMark", subjectData.MAT104.TimeStamp, '/users/' + user.uid + '/classes/MAT104', subjectClass.MAT104.ClassHeld + 1);
                                     $("#mat104AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/MAT104', subjectData.MAT104.Attendance + 1, "#mat104AtteMark", subjectClass.MAT104.ClassHeld);
@@ -119,7 +119,7 @@ $(document).ready(function() {
                             }
                             if (subjectData.PHY102) {
                                 init(".phy102", "#phy102AttePercentage", "#phy102ProgressBar", subjectData.PHY102.Attendance, subjectClass.PHY102.ClassHeld);
-                                if ((date.getDay() == 2 || date.getDay() == 4) && ((date.getHours() == 20) || (date.getHours() == 21 && date.getMinutes() == 0))) {
+                                if ((date.getDay() == 2 && (date.getHours() == 21)) || (date.getDay() == 5 && (date.getHours() == 22))) {
                                     initButton("#phy102AtteMark", subjectData.PHY102.TimeStamp, '/users/' + user.uid + '/classes/PHY102', subjectClass.PHY102.ClassHeld + 1);
                                     $("#phy102AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/PHY102', subjectData.PHY102.Attendance + 1, "#phy102AtteMark", subjectClass.PHY102.ClassHeld);
@@ -131,7 +131,7 @@ $(document).ready(function() {
                             }
                             if (subjectData.CSD204) {
                                 init(".csd204", "#csd204AttePercentage", "#csd204ProgressBar", subjectData.CSD204.Attendance, subjectClass.CSD204.ClassHeld);
-                                if ((date.getDay() == 2 || date.getDay() == 5) && ((date.getHours() == 21) || (date.getHours() == 22 && date.getMinutes() == 0))) {
+                                if ((date.getDay == 5 && (date.getHours() == 20))) {
                                     initButton("#csd204AtteMark", subjectData.CSD204.TimeStamp, '/users/' + user.uid + '/classes/CSD204', subjectClass.CSD204.ClassHeld + 1);
                                     $("#csd204AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/CSD204', subjectData.CSD204.Attendance + 1, "#csd204AtteMark", subjectClass.CSD204.ClassHeld);
@@ -143,7 +143,7 @@ $(document).ready(function() {
                             }
                             if (subjectData.EED205) {
                                 init(".eed205", "#eed205AttePercentage", "#eed205ProgressBar", subjectData.EED205.Attendance, subjectClass.EED205.ClassHeld);
-                                if ((date.getDay() == 1 || date.getDay() == 4) && ((date.getHours() == 20) || (date.getHours() == 21 && date.getMinutes() == 0))) {
+                                if ((date.getDay() == 2 && (date.getHours() == 22)) || (daye.getDay() == 3 && (date.getHours() == 22)) || (date.getDay() == 5 && (date.getHours() == 22))) {
                                     initButton("#eed205AtteMark", subjectData.EED205.TimeStamp, '/users/' + user.uid + '/classes/EED205', subjectClass.EED205.ClassHeld + 1);
                                     $("#eed205AtteMark").on('click', function() {
                                         markAttendance('/users/' + user.uid + '/classes/EED205', subjectData.EED205.Attendance + 1, "#eed205AtteMark", subjectClass.EED205.ClassHeld);
